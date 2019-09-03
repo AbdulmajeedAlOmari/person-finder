a320aaa8d525c6dd8d85d8905aeb1e930fe68a28
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2019-09-03 12:04:10 +0300

    Initial commit

---
+++ src/main/java/dev/alomari/person/finder/PersonFinderApplication.java
@@ -0,0 +1,13 @@
+package dev.alomari.person.finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonFinderApplication.class, args);
+    }
+
+}
